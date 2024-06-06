878f4a28144d942a49852c8382428be4efdc5124
RafaAyGar <rafaaylloningeniero@gmail.com>
2024-06-06 09:00:26 +0200

    update

--- aeon/classification/deep_learning/_lite_time.py
+++ aeon/classification/deep_learning/_lite_time.py
@@ -9,11 +9,10 @@ import time
 from copy import deepcopy
 
 import numpy as np
-from sklearn.utils import check_random_state
-
 from aeon.classification.base import BaseClassifier
 from aeon.classification.deep_learning.base import BaseDeepClassifier
 from aeon.networks import LITENetwork
+from sklearn.utils import check_random_state
 
 
 class LITETimeClassifier(BaseClassifier):
